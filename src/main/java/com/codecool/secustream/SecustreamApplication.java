55b66cc5e72e38132c8c8dd610fb564099141aaa
minimalgeek <minimalgeek@gmail.com>
2020-03-24 21:06:39 +0100

    add every dependency, and basic inmemory H2 url

---
+++ src/main/java/com/codecool/secustream/SecustreamApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.secustream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecustreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecustreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/secustream/SecustreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.secustream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecustreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
